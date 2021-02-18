dacea053747cfdf9452b23159596870d8a0bf1cc
thaylla <thayllachristineo@gmail.com>
2021-02-18 09:25:00 -0300

    feat: create GlobalStyle and add Roboto font

---
+++ client/src/styles/GlobalStyle.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Globalstyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default Globalstyle;
