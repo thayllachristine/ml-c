b96a8dfc78c90638038b7b7a16c76cbebc3dbdc0
thaylla <thayllachristineo@gmail.com>
2021-02-19 11:34:40 -0300

    feat: add cors into apis

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import searchController from './controllers/search.controller';
 import detailsController from './controllers/details.controller';
@@ -9,6 +10,8 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use(searchController);
 app.use(detailsController);
