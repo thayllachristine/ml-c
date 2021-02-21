0d7c30c817b8241851a6cd7724966df18515dc38
thaylla <thayllachristineo@gmail.com>
2021-02-21 19:37:32 -0300

    feat(spinner): create a wrapper from component oustide

--- client/src/components/Spinner/Spinner.styles.tsx
+++ client/src/components/Spinner/Spinner.styles.tsx
@@ -1,6 +1,13 @@
 import styled, { keyframes } from 'styled-components';
 import color from '@styles/colors';
 
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  height: 60vh;
+  justify-content: center;
+`;
+
 const rotate360 = keyframes`
   from {
     transform: rotate(0deg);
@@ -10,7 +17,7 @@ const rotate360 = keyframes`
   }
 `;
 
-const Wrapper = styled.div`
+const Content = styled.div`
   animation: ${rotate360} 1s linear infinite;
   background: transparent;
   border-bottom: 4px solid ${color.yellow};
@@ -18,9 +25,9 @@ const Wrapper = styled.div`
   border-radius: 50%;
   border-right: 4px solid ${color.yellow};
   border-top: 4px solid ${color.yellow};
-  height: 24px;
+  height: 48px;
   transform: translateZ(0);
-  width: 24px;
+  width: 48px;
 `;
 
-export { Wrapper };
+export { Wrapper, Content };
--- client/src/components/Spinner/index.tsx
+++ client/src/components/Spinner/index.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from 'react';
 import * as S from './Spinner.styles';
 
-const Spinner: FC = (): JSX.Element => <S.Wrapper />;
+const Spinner: FC = (): JSX.Element => (
+  <S.Wrapper>
+    <S.Content />
+  </S.Wrapper>
+);
 
 export default Spinner;
