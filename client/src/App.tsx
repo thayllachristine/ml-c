75f050deb102b5b319f481fbb5ab8cfa6542b28a
thaylla <thayllachristineo@gmail.com>
2021-02-21 19:45:09 -0300

    feat(app): create routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,30 @@
 import React, { FC } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { AppProvider } from '@contexts/AppContext';
+
+import ItemDetails from '@pages/ItemDetails';
+import ItemsResult from '@pages/ItemsResult';
+
+import Globalstyle from '@styles/GlobalStyle';
+
+import Search from '@components/Search';
 
 const App: FC = (): JSX.Element => {
-  return <></>;
+  return (
+    <AppProvider>
+      <Router>
+        <Globalstyle />
+        <Search />
+
+        <Switch>
+          <Route exact path="/items" render={() => <ItemsResult />} />
+          <Route exact path="/items/:id" render={() => <ItemDetails />} />
+          <Route exact path="/" />
+        </Switch>
+      </Router>
+    </AppProvider>
+  );
 };
 
 export default App;
