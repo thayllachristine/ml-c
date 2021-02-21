4d141a4325ade687056428c74eb7fc508cbc10fd
thaylla <thayllachristineo@gmail.com>
2021-02-21 19:43:42 -0300

    feat(content shared): remove page

--- client/src/pages/ContentShared/ContentShared.styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import color from '../../styles/colors';
-
-export const Wrapper = styled.div`
-  background-color: ${color.white};
-  border-radius: 4px;
-  padding: 16px;
-`;
